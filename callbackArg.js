a2ae12275250951f8bfa8f8564c14e0a2251b996
Nikolai <nikolaigauer@gmail.com>
2017-06-29 20:17:38 +0000

    Refactored the original findWaldo function, see comments for detail

--- callbackArg.js
+++ callbackArg.js
@@ -1,24 +1,31 @@
 
-function findWaldo(arr, found) {
- // index = arr[i]   <----------------------------- I thought I needed to declare a variable called index, but this was not needed
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-       found(i);   // execute callback
+// Below is the original for loop which has been refactored into a forEach function
+
+// function findWaldo(arr, found) {
+//  // index = arr[i]                     <----------------------------- I thought I needed to declare a variable called index, but this was not needed
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//        found(i);                                       // execute callback, added the i (index number?)
+//     }
+//   }
+// }
+
+var findWaldo = function(arr, found) {                  // refactored the above for loop to a forEach funciton
+  arr.forEach(function(name, index) {
+    if (name === "Waldo") {                           // when the `name` "Waldo" is found, execute the `found` function which calls the `actionWhenFound` function
+      found(index);
     }
-  }
+
+  });
 }
+
 function actionWhenFound(index) {             // modified the function to take the index, it is still a mystery to me where/how the index number is coming from
   console.log("Found him!");
-  console.log("Found Waldo at index " + index + "!") // added a new concole.log to print
-};
+  console.log("Found Waldo at index " + index + "!"); // added a new concole.log to print
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound); //
 
 
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-/*
-assignment:
-Did you know that we can pass results via callback functions?
 
-Modify the callback function in the previous example so that logs the index of the array where Waldo is found, ie. "Found Waldo at index 2!". (You will need to modify actionWhenFound to receive the index.)
-Once you have this working, remember to commit your changes using Git. A good commit message should be concise and descriptive of the changes you have made. An example commit message: Print the index of Waldo when found.
-*/
