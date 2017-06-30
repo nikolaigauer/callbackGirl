9a8fa4ab5601c6603b03169b82f005d92868f3d8
Nikolai <nikolaigauer@gmail.com>
2017-06-30 02:03:02 +0000

    here's a functional map function, look inside for detailed notes

--- callbackMaps.js
+++ callbackMaps.js
@@ -1,20 +1,24 @@
-var words = ["Coffee", "Tea", "sourMilk", "", "spoiledKambucha"];
+var words = ["wurd", "WOOOORDS", "WORDS", "weeerd", "Wordy"]; // this is the array I want to map
 
-function map(arrToMap, cbFunc) {
-  words = arrToMap
-  var resultsArr = [];
-  for (var i = 0; i < arrToMap.length; i++) {
-    resultsArr.push(cbFunc(arrToMap[i], i, arrToMap));
+function map(arrToMap, cbFunc) {                          // created the function `map` with the two arguments called for in the assignment
+  var resultsArr = [];                                   // created and empty array to push the results of the function `cbFunc`
+  for (var i = 0; i < arrToMap.length; i++) {           // this for loop runs for the length of the array input
+    resultsArr.push(cbFunc(arrToMap[i], i, arrToMap));  // this is somewhat mysterious to me, but I know that `.push` is pushing the counting results of the function `cbFunc`
   }
-  return resultsArr;
-
+  return resultsArr;                                     // this stops the function
 }
-console.log(map)
+
+// below I am declaring the variable `answer` to hold the return of the function map,
+// the array `words` have been plotted in to be manipulated in the `map` function
+var answer = map(words, function(word) {
+  return word.length;                   // this counts the characters of the individual words
+})
+
+console.log(answer) // prints the answer to the assignment to the terminal
 
 
-// map(words, function(word) {
-//   return word.length;
-// });
+
+
 
 // // Exercise
 // // Implement your own version of the built-in array map function.
